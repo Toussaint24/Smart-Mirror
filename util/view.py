1116202d7522ccbd826082e6294b6b87027c25f9
Jayden Toussaint <jaydenr.toussaint@gmail.com>
2024-02-02 21:55:06 -0600

    Added X button for all views

--- util/view.py
+++ util/view.py
@@ -12,4 +12,6 @@ class View(ctk.CTkFrame):
         self.view_list[name] = self
         self.grid(row=0, column=0, sticky=tk.NSEW)
     
-    
+    def _init_widgets(self):
+        self.button = ctk.CTkButton(self, text="X", fg_color="red", text_color="white", command=self._controller.quit)
+        self.button.place(relx=1.0, rely=0.0, anchor="ne", relwidth=0.075, relheight=0.05)
