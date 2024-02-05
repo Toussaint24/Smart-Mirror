58b82a8474e2621b7e898d61b8c42dec03cacc52
Jayden Toussaint <jaydenr.toussaint@gmail.com>
2024-02-05 14:54:46 -0600

    Fixed crashing on failing to obtain frame

--- util/view.py
+++ util/view.py
@@ -4,10 +4,9 @@ import customtkinter as ctk
 
 
 class View(ctk.CTkFrame):
-    view_list = {}
     def __init__(self, name: str, parent: tk.Tk):
         super().__init__(parent)
         self._parent = parent
-        self.view_list[name] = self
+        self._parent.view_list[name] = self
         self.pack(expand=True, fill="both")
         self.pack_propagate(False)
