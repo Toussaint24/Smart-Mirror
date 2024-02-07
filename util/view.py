86bca11fd3a4a46baaddb1f589ce728d665eb505
Jayden Toussaint <jaydenr.toussaint@gmail.com>
2024-02-06 22:19:31 -0600

    Added X button in top left

--- util/view.py
+++ util/view.py
@@ -10,3 +10,7 @@ class View(ctk.CTkFrame):
         self._parent.view_list[name] = self
         self.place(relwidth=1.0, relheight=1.0)
         self.pack_propagate(False)
+        
+    def _init_widgets(self):
+        self.button = ctk.CTkButton(self, text="X", fg_color="red", text_color="white", command=self._parent.quit)
+        self.button.place(relx=1.0, rely=0.0, anchor="ne", relwidth=0.075, relheight=0.05)
