12dc31afbad9c9ec1ac1822f103085c3fc3f620c
Jayden Toussaint <jaydenr.toussaint@gmail.com>
2024-02-06 22:20:07 -0600

    Removed unnecessary imports

--- main.py
+++ main.py
@@ -1,13 +1,3 @@
-import tkinter as tk
-import customtkinter as ctk
+from fitnessmonitor.app import App
 
-from presenterai.app import App
-from fingertracker.fingertracker import FingerTracker
-
-finger_tracker = FingerTracker((1440, 900))
-finger_tracker.init()
-
-while True:
-    finger_tracker.move_cursor()
-
-"""App()"""
+App()
