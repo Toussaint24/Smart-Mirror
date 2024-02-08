568f15ff8a30bed576a1e6021332c7a4f7bd9257
Jayden Toussaint <jaydenr.toussaint@gmail.com>
2024-02-07 18:10:26 -0600

    Changed for openai testing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import tkinter as tk
 import customtkinter as ctk
+from openai import OpenAI
+
+client = OpenAI()
 
 from presenterai.app import App
 from fingertracker.fingertracker import FingerTracker
@@ -10,5 +13,17 @@ from fingertracker.fingertracker import FingerTracker
 # while True:
 #     finger_tracker.move_cursor()
 
+"""prompt = "Can you edit my essay?"
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        {
+            "role":"user",
+            "content":prompt
+        }
+    ],
+    model="gpt-3.5-turbo"
+)
+
+print(chat_completion)"""
 App()
-"""App()"""
